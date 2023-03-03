9ba1753da2d73904ed5ca36528beee3ba8e64835
ihmoon <ihmoon>
2023-03-03 15:59:57 +0900

    start

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { type NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <div className="text-2xl text-red-200">
+        world
+      </div>
+    </>
+  );
+};
+
+export default Home;
